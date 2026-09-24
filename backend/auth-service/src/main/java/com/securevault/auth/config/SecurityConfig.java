8e5258f78d7053b05b1bb0edb6fc87694ac010df
ayoubbng <AYOUBBOUNAGA2001@gmail.com>
2026-09-24 01:57:19 +0000

    feat(docker): add multi-stage Dockerfiles + docker-compose orchestration

--- backend/auth-service/src/main/java/com/securevault/auth/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/securevault/auth/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpMethod;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -30,6 +32,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 "/api/auth/login",
                 "/api/auth/2fa/enable",
                 "/api/auth/2fa/verify",
+		"/actuator/**",
                 "/api/auth/2fa/reset"
             ).permitAll()
             .anyRequest().authenticated()
--- backend/vault-service/src/main/java/com/securevault/vault/config/SecurityConfig.java
+++ backend/vault-service/src/main/java/com/securevault/vault/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     // @Bean
@@ -30,6 +32,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+		    "/actuator/**",
                     "/api/vault/**"
                 ).permitAll()
                 .anyRequest().authenticated()
